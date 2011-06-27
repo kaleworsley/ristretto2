f46a26ba7ea0b26909bff7056ec4e312a5b764ad
Kale Worsley <kale@egressive.com>
2011-06-28 11:45:04 +1200

    Adds show_flash helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def show_flash
+    content_tag :div, nil, { :id => 'flash' } do
+      flash.collect { |index, value| content_tag(:div, value, {:id => "flash-#{index.to_s}"}) }.join.html_safe
+    end
+  end
+
 end
