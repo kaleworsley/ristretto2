ae5fb334b58ad886b1f909f819a02de71c0770ef
Kale Worsley <kale@egressive.com>
2011-06-29 10:35:19 +1200

    Adds navigation partial and some application helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,31 @@ module ApplicationHelper
     end
   end
 
+  def link_to_with_active(name, options = {}, html_options = {}, &block)
+    if current_page?(options)
+      class_array = html_options[:class].to_s.split(' ')
+      class_array.push('active')
+      html_options[:class] = class_array.join(' ')
+    end
+    link_to name, options, html_options, &block
+  end
+
+  def title(page_title, show_title = true)
+    content_for(:title) { h(page_title.to_s) }
+    @show_title = show_title
+  end
+
+  def show_title?
+    @show_title
+  end
+
+  def stylesheet(*args)
+    content_for(:head) { stylesheet_link_tag(*args) }
+  end
+
+  def javascript(*args)
+    content_for(:head) { javascript_include_tag(*args) }
+  end
+
 end
+
