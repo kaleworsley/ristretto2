e59f151245a07d6fd0d397bd48b3754bb7743ca5
Kale Worsley <kale@egressive.com>
2011-07-18 14:56:11 +1200

    Fixes dashboard tests.

--- test/functional/dashboard_controller_test.rb
+++ test/functional/dashboard_controller_test.rb
@@ -1,9 +1,20 @@
 require 'test_helper'
 
 class DashboardControllerTest < ActionController::TestCase
-  test "should get show" do
+  setup do
+    @user = Factory.create(:user)
+  end
+
+  test "should not show dashboard if not logged in" do
+    get :show
+    assert_redirected_to new_user_session_url
+  end
+
+  test "should show dashboard if logged in" do
+    sign_in @user
     get :show
     assert_response :success
   end
 
 end
+
