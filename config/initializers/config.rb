345cf07af4806576dc0235b86146806581db0488
Kale Worsley <kale@egressive.com>
2011-07-28 14:33:23 +1200

    Fixes deprecation warning for RAILS_ROOT.

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,4 +1,4 @@
 CONFIG = {}
-CONFIG = YAML.load_file("#{RAILS_ROOT}/config/config.yml")[RAILS_ENV] if File.exists?("#{RAILS_ROOT}/config/config.yml")
+CONFIG = YAML.load_file("#{::Rails.root.to_s}/config/config.yml")[RAILS_ENV] if File.exists?("#{::Rails.root.to_s}/config/config.yml")
 CONFIG[:git_revision] = `git rev-parse HEAD 2>/dev/null`.to_s.strip
 
