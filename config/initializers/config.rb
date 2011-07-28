0ca0142914adbe0cd902579d369f767952512576
Kale Worsley <kale@egressive.com>
2011-07-28 15:16:41 +1200

    Fixes deprecation warning for RAILS_ENV.

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,4 +1,6 @@
-CONFIG = {}
-CONFIG = YAML.load_file("#{::Rails.root.to_s}/config/config.yml")[RAILS_ENV] if File.exists?("#{::Rails.root.to_s}/config/config.yml")
-CONFIG[:git_revision] = `git rev-parse HEAD 2>/dev/null`.to_s.strip
+config = {}
+config = YAML.load_file("#{::Rails.root.to_s}/config/config.yml")[::Rails.env] || {} if File.exists?("#{::Rails.root.to_s}/config/config.yml")
+config[:git_revision] = `git rev-parse HEAD 2>/dev/null`.to_s.strip
+
+CONFIG = config
 
