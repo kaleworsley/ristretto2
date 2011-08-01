3bbb9f5a6f59f2d4ebf8f73d03c578a3e19b8327
Kale Worsley <kale@egressive.com>
2011-08-02 10:54:27 +1200

    Adds customer integration tests.

---
+++ test/integration/customer_test.rb
@@ -0,0 +1,86 @@
+require 'integration_test_helper'
+
+class UserTest < ActionController::IntegrationTest
+  def setup
+    @user = Factory.create(:user, :email => 'test@example.com', :password => 'password')
+    @customer_attributes = {
+      "units_attributes" => {
+        "0" => {
+          "name" => "Test Customer",
+          "position" => "0",
+          "postal_address" => "123 Fake St\r\nSomeplace",
+          "physical_address" => "123 Fake St\r\nSomeplace",
+          "phones_attributes" => {
+            "0" => {
+              "label" => "Main",
+              "number" => "555 5555"
+            }
+          }
+        }
+      }
+    }
+    @customer = Factory.create(:customer, @customer_attributes)
+  end
+
+  test "create customer" do
+    sign_in 'test@example.com', 'password'
+    visit '/customers/new'
+    assert_equal '/customers/new', page.current_path
+
+    fill_in 'Name', :with => 'New Customer'
+    fill_in 'Physical address', :with => '123 Fake St'
+    fill_in 'Postal address', :with => '123 Fake St'
+    fill_in 'Label', :with => 'Main Number'
+    fill_in 'Number', :with => '555 5555'
+
+    click_button 'Create Customer'
+
+    assert_equal '/customers/new-customer', page.current_path
+    assert page.has_content? 'Customer was successfully created.'
+    assert page.has_content? 'New Customer'
+    assert page.has_content? 'Name: New Customer'
+    assert page.has_content? 'Physical Address: 123 Fake St'
+    assert page.has_content? 'Postal Address: 123 Fake St'
+    assert page.has_content? 'Main Number: 555 5555'
+  end
+
+  test "customer list" do
+    sign_in 'test@example.com', 'password'
+    visit '/customers'
+    assert page.has_content? 'Listing customers'
+    assert page.has_content? 'Test Customer'
+  end
+
+  test "edit customer" do
+    sign_in 'test@example.com', 'password'
+    visit '/customers'
+    assert page.has_content? 'Test Customer'
+    click_link 'Edit'
+    assert_equal '/customers/test-customer/edit', page.current_path
+
+    assert page.has_field? 'Name', :with => 'Test Customer'
+    assert page.has_field? 'Physical address', :with => "123 Fake St\r\nSomeplace"
+    assert page.has_field? 'Postal address', :with => "123 Fake St\r\nSomeplace"
+    assert page.has_field? 'Label', :with => 'Main'
+    assert page.has_field? 'Number', :with => '555 5555'
+
+    fill_in 'Name', :with => 'New Customer'
+    fill_in 'Physical address', :with => "123 Fake St\r\nSome other place"
+    fill_in 'Postal address', :with => "123 Fake St\r\nSome other place"
+    click_button 'Update Customer'
+    assert page.has_content? 'Name: New Customer'
+    assert page.has_content? "Physical Address: 123 Fake St Some other place"
+    assert page.has_content? "Postal Address: 123 Fake St Some other place"
+    assert page.has_content? 'Customer was successfully updated.'
+  end
+
+  test "delete customer" do
+    sign_in 'test@example.com', 'password'
+    visit '/customers'
+    page.has_content? 'New Customer'
+    click_link 'Destroy'
+    assert_equal '/customers', page.current_path
+    page.has_no_content? 'New Customer'
+  end
+end
+
